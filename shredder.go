db71acb38c0178fe354d5dc1d8b86f8c9d254e9d
leonardoandrade <leonardo@andrade.io>
2014-07-27 14:22:45 +0200

    build TF/IDF map and write to file

--- shredder.go
+++ shredder.go
@@ -3,32 +3,49 @@ package wordstats
 import (
 	"fmt"
 	"strconv"
-
+	"strings"
 )
 
 type Shredder struct {
-	termCount int
+	termCount     int
 	documentCount int
+	stopped       bool
+	wordStats     *WordStats
 }
 
-func newShredder() (*Shredder) {
-	return &Shredder{0,0}
+func newShredder(wordStats *WordStats) *Shredder {
+	return &Shredder{0, 0, true, wordStats}
 }
 
 func (this *Shredder) worker(textChannel chan string) {
-	for text := range(textChannel) {
+	this.stopped = false
+	for text := range textChannel {
 		//fmt.Println("doclen:"+strconv.Itoa(len(text)))
 		this.termCount = this.termCount + len(text)
 		this.documentCount++
+		tmp := strings.Split(strings.Replace(strings.ToLower(text), "\n", "", -1), " ")
+
+		for _, tok := range tmp {
+			this.wordStats.setTF(tok, this.wordStats.TF(tok)+1)
+			//this.wordStats.stats[tok].idf = this.wordStats.stats[tok].idf + 1
+		}
+		if this.documentCount > 3000 {
+			break
+		}
 	}
+	this.stopped = true
 }
 
 func (this *Shredder) Run(textChannel chan string, numThreads int) {
-	for i := 0; i< numThreads; i++ {
+	for i := 0; i < numThreads; i++ {
 		go this.worker(textChannel)
 	}
 }
 
 func (this *Shredder) Report() {
-	fmt.Println("docs: "+strconv.Itoa(this.documentCount)+" terms: "+strconv.Itoa(this.termCount))
+	fmt.Println("docs: " + strconv.Itoa(this.documentCount) + " terms: " + strconv.Itoa(this.termCount))
+}
+
+func (this *Shredder) Stopped() bool {
+	return this.stopped
 }
--- wordstats.go
+++ wordstats.go
@@ -1,49 +1,75 @@
 package wordstats
 
 import (
+	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
 type Language int
 
-const(
-  DE = iota
-  PT
-  EN
+const (
+	DE = iota
+	PT
+	EN
 )
 
 type termStats struct {
-  tf int
-  idf float32
+	tf  int
+	idf float64
 }
 
 type WordStats struct {
-   stats map[string]termStats
+	stats map[string]termStats
+}
+
+func (this *WordStats) dumpToFile(fileName string) {
+	f, _ := os.Create(fileName)
+	for k, v := range this.stats {
+		f.WriteString(k + ";" + strconv.Itoa(v.tf) + ";" + fmt.Sprintf("%.6f", v.idf) + "\n")
+	}
+	f.Close()
 }
 
 func ParseWikipedia(fileName string, language Language, outputTermFile string) {
 	textChannel := make(chan string)
 	go parseFile(fileName, textChannel)
-
-	shredder := newShredder()
+	ws := &WordStats{make(map[string]termStats)}
+	shredder := newShredder(ws)
 	shredder.Run(textChannel, 4)
 	for {
 		time.Sleep(1 * time.Second)
 		shredder.Report()
+		if shredder.Stopped() {
+			break
+		}
+	}
+	ws.dumpToFile(outputTermFile)
+}
+
+func NewWordStats(termFile string) *WordStats {
+	//TODO
+	return nil
+}
+
+func (this *WordStats) TF(term string) int {
+	if val, ok := this.stats[term]; ok {
+		return val.tf
+	} else {
+		return 0
 	}
 }
 
-func NewWordStats(termFile string) (*WordStats) {
-  //TODO
-  return nil
+func (this *WordStats) IDF(term string) float32 {
+	//TODO
+	return 0.0
 }
 
-func (this *WordStats) TF(term string) (int){
-  //TODO
-  return 0
-}
-
-func (this *WordStats) IDF(term string) (float32){
-  //TODO
-  return 0.0
+func (this *WordStats) setTF(key string, tf int) {
+	if val, ok := this.stats[key]; ok {
+		val.tf = tf
+	} else {
+		this.stats[key] = termStats{tf, 0.0}
+	}
 }
